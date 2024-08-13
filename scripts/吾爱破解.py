8d0d2159382f9784c220ab746df5fa74cfbe4ea9
master <84203689+cloudcranes@users.noreply.github.com>
2024-08-13 14:31:51 +0800

    Update 吾爱破解.py

--- scripts/吾爱破解.py
+++ scripts/吾爱破解.py
@@ -6,21 +6,16 @@
 # const $ = new Env("吾爱破解")
 import re
 from datetime import datetime
-from os import environ
-from sys import stdout
 import requests
-from SendNotify import send
+from send import send
+from qinglong import ql
 
-现在 = datetime.now()
+now = datetime.now()
+
+
 # ck获取 https://www.52pojie.cn/forum.php
 # ck格式 完整cookie
-wapj_ck = environ.get('wapj_ck') if environ.get('wapj_ck') else True
-
-
-def print_now(content):
-    print(content)
-    msg.append(content)
-    stdout.flush()
+# wapj_data = 'htVC_2132_visitedfid=24; Hm_lvt_46d556462595ed05e05f009cdafff31a=1717219682,1717507150; htVC_2132_saltkey=g7037uws; htVC_2132_lastvisit=1717489702; wzws_sessionid=gDM2LjU2Ljg0LjE3MoJkYjFjYWGgZl8UWoE1NGU1YTQ=; Hm_lpvt_46d556462595ed05e05f009cdafff31a=1717507160; htVC_2132_seccodecSRKk=1939482.ed497f50ec2e24ff25; htVC_2132_ulastactivity=1717507149%7C0; htVC_2132_auth=aa9ddejd4OTFj8o9Fm2LYd17HhwJmNgeH6ROae23BIuTzhCS0W2Iwd5%2BQuBcrlh3lO59ZONsMpate2EGNS9eijVphOSf; htVC_2132_lastcheckfeed=2002841%7C1717507149; htVC_2132_checkfollow=1; htVC_2132_lip=36.56.84.172%2C1717507149; htVC_2132_lastact=1717507154%09misc.php%09seccode; htVC_2132_connect_is_bind=1; htVC_2132_seccodecS=1939503.27fc6acc25ca6ab0ac'
 
 
 class wapj:
@@ -32,7 +27,7 @@ class wapj:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         }
         self.host = 'https://www.52pojie.cn'
-        self.main()
+        self.msg = '##【吾爱破解】\n'
 
     def get_user_info(self):
         url = self.host + '/home.php?mod=spacecp&ac=credit&showcredit=1'
@@ -45,10 +40,12 @@ class wapj:
             # </ul><ul class="creditl mtm bbda cl"><li class="xi1 cl"><em> 吾爱币:</em>437 CB &nbsp; <a href="home.php?mod=spacecp&amp;ac=credit&amp;op=buy" class="xi2">捐助&raquo;</a></li>
             name = re.findall(r'target="_blank" title="访问我的空间">(.*?)</a></strong>', res.text)[0]
             points = re.findall(r'吾爱币: </em>(\d+)', res.text)[0]
-            print_now(f'用户名：{name} 吾爱币: {points}')
+            # 格式化输出
+            self.msg += f'用户：{name}\n'
+            self.msg += f'吾爱币：{points}\n'
             return points
         except:
-            print_now('查询吾爱币失败')
+            self.msg += '查询吾爱币失败\n'
             return None
 
     def sign(self):
@@ -67,7 +64,7 @@ class wapj:
         try:
             res = requests.get(url=url, headers=headers, timeout=10)
         except Exception as e:
-            print_now(f'签到失败 {e}')
+            self.msg += f'签到失败 {e}\n'
 
     def judge_sign(self):
         url = self.host + '/forum.php'
@@ -88,38 +85,31 @@ class wapj:
             # 根据正则规则匹配返回的html语言，判断是否签到成功
             # <a href="home.php?mod=task&amp;do=draw&amp;id=2&amp;referer=https%3A%2F%2Fwww.52pojie.cn%2Fhome.php%3Fmod%3Dtask%26do%3Dapply%26id%3D2" class="xi2">已签到</a>
             if re.findall(r'https://static.52pojie.cn/static/image/common/wbs.png', res.text):
-                print_now('签到成功')
                 return True
             else:
-                print_now('签到失败')
                 return False
-        except:
-            print_now(f'签到失败')
+        except Exception as e:
+            print(e)
             return False
 
     def main(self):
-        if self.judge_sign():
+        if not self.judge_sign():
             self.sign()
         else:
-            print_now('今日已签到')
+            self.msg += '今日已签到\n'
         self.get_user_info()
 
 
 if __name__ == '__main__':
-    ckArr = []
-    for ck in wapj_ck.split('&'):
-        if len(ck) > 10:
-            ckArr.append(ck)
-    print('共' + str(len(ckArr)) + '个账户')
-    c = 0
-    u = []
-    msg = []
-    for i in ckArr:
-        c += 1
-        print(f"\n****************** 开始账号 {c} ******************\n")
-        msg.append(f"\n******** 账号 {c} ********\n")
-        wapj(c)
-    print("\n****************** 结束 ******************\n")
-    print('\n'.join(msg))
-    send('吾爱破解', '\n'.join(msg))
-    exit(0)
+    ql = ql()
+    envs = ql.get_env_by_name('wapj_data')
+    for env in envs:
+        ck = env['value']
+        remarks = env.get('remarks', '')
+        run = wapj(ck)
+        run.main()
+        if remarks and '@' in remarks:
+            send.wxpusher(remarks.split('@')[1], run.msg)
+        else:
+            print('未配置wxpusher')
+        print(run.msg)
